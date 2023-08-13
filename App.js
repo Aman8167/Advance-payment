d63c111934390438559b2c2de4502b1597277484
Aman8167 <118344734+Aman8167@users.noreply.github.com>
2023-08-13 16:53:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Responsive2 from './Component/Responsive2';
+
+function App() {
+  return (
+    <div className='App'>
+<Responsive2 />
+    </div>
+  );
+}
+
+export default App;
---
+++ Responsive2.js
@@ -0,0 +1,124 @@
+import React from 'react'
+import './Responsive2.css';
+function Responsive1() {
+  return (
+    
+        <div className="container-fluid">
+               
+      <div className="card-1">
+        <div className="row justify-content-center">
+            <div className="col-md-10">
+        <div className="card-body mt-5">
+          <div className="form-group">
+            
+            <input 
+              type="text"
+            className="form-control  "
+            placeholder="Depositer name- " style={{height:'3rem'}}
+            />
+          </div><br></br>
+          <div className="form-group">
+            
+            <input 
+              type="text"
+            className="form-control  "
+            placeholder="Client name Automatically come- " style={{height:'3rem'}}
+            />
+          </div><br></br>
+
+          <div className="form-group">
+            
+            <input 
+              type="text"
+            className="form-control  "
+            placeholder="Amount-" style={{height:'3rem'}}
+            />
+          </div><br></br>
+          <div className="form-group ">
+           
+            <input type="Date" 
+              className="form-control  "
+              placeholder="First select date" style={{height:'3rem'}}
+            />
+          </div>
+          <br></br>
+          <div className="form-group ">
+           
+            <input type="text" 
+              className="form-control     "
+              placeholder="Advance Payment-
+               "
+             
+               style={{height:'3rem',}}
+               
+            />
+            
+           
+          </div>
+          <br></br>
+          <div className="form-group  ">
+         
+            <input type="text" 
+              className="form-control  " 
+            
+              placeholder="Remaining payment-"
+               style={{height:'3rem' }}
+            />
+          </div>
+          <br></br>
+          <div className="form-group  ">
+         
+            <input type="text" 
+              className="form-control  " 
+            
+              placeholder="Event name-"
+               style={{height:'3rem' }}
+            />
+          </div>
+          <br></br>
+          <div className="form-group ">
+           
+           <input type="text " 
+             className="form-control     "
+             placeholder="Payment details-
+              "
+            
+              style={{height:'6rem',}}
+              
+           />
+           
+          
+         </div>
+          
+          <br></br>
+          <div className="form-group ">
+           
+           <input type="text " 
+             className="form-control     "
+             placeholder="COMPLETE CHART-
+              "
+            
+              style={{height:'6rem',}}
+              
+           />
+           
+          
+         </div>
+          
+          <br></br>
+          <button id="btn1" className="btn btn-primary" >
+            Add
+          </button>
+        
+          
+
+        </div>
+</div>
+    </div>
+      </div>
+    </div>
+    
+  )
+}
+
+export default Responsive1;
